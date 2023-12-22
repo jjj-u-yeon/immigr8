8c9da1f4b06c453c145eb6b60be95dffe82375cd
juyeoning <jhong.proj@gmail.com>
2023-12-22 03:22:37 -0500

    frontend spacing

--- oa/src/Form.js
+++ oa/src/Form.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect, Link } from 'react'
-import { FormControl, FormControlLabel, FormLabel, Grid,RadioGroup,TextField, Radio } from '@mui/material';
+import { Paper,FormControl, FormControlLabel, FormLabel, Grid,RadioGroup,TextField, Radio, Select, Tooltip, IconButton } from '@mui/material';
+import {HelpOutline} from '@mui/icons-material';
 import { useForm } from './components/useForm';
 import {useNavigate, Router, Routes, BrowserRouter} from 'react-router-dom'
 import { Button } from './components/Button'
 import { setForm } from './services/formService'
+import states from './consts/states.json'
+import './App.css';
+import {DateField} from '@mui/material'
 
 const initialValues = {
     givenNameA: '',
     middleNameA: '',
     familyNameA: '',
-    givenNameC: '',
-    middleNameC: '',
-    familyNameC: '',
+    givenName: '',
+    middleName: '',
+    familyName: '',
     email: '',
-    mobile: '',
+    phone: '',
+    street: '',
     city: '',
-    gender: 'male',
+    state: '',
     houseType: 'House',
+    unitNumber: '',
+    zipCode: '',
+    creditNum: '',
+    creditType: '',
     creditExpiry: new Date(),
+    authorizedAmount: '',
 }
 
 export default function Form() {
@@ -57,10 +67,13 @@ export default function Form() {
     }
 
     return (
-        <form margin={1} onSubmit={handleSubmit}>
-            <Grid container>
-                <Grid item xs={6}></Grid>
+        <Paper className='Paper' elevation={5}>
+        <form spacing={2} onSubmit={handleSubmit}>
+            <Grid >
+                <h3>Applicant's/Petitioner's/Requester's Information</h3>
+                <Grid  xs={12} className='input'>
                     <TextField
+                    required={true}
                     variant='outlined'
                     label='Given Name'
                     name='givenNameA'
@@ -69,17 +82,71 @@ export default function Form() {
                     />
                     <TextField
                     variant='outlined'
+                    label='Middle Name (if any)'
+                    name='middleNameA'
+                    value={values.middleNameA}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    required={true}
+                    variant='outlined'
                     label='Family Name'
                     shrink={true}
                     name='familyNameA'
                     value={values.familyNameA}
                     onChange={handleInputChange}
                     />
-                <Grid item xs={6}>
+                    </Grid>
+                <h3>Credit Card Billing Information</h3>
+                <Tooltip title="Credit Card Holder's Name as it appears on the card" placement='bottom'>
+                    <HelpOutline/>
+                </Tooltip>
+                <Grid item xs={12} className='input'>
+                <TextField
+                    required={true}
+                    variant='outlined'
+                    label='Given Name'
+                    name='givenName'
+                    value={values.givenName}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    variant='outlined'
+                    label='Middle Name (if any)'
+                    name='middleName'
+                    value={values.middleName}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    required={true}
+                    variant='outlined'
+                    label='Family Name'
+                    shrink={true}
+                    name='familyName'
+                    value={values.familyName}
+                    onChange={handleInputChange}
+                    />
+                </Grid>
+                <Grid item xs={12} className='input'>
+                <TextField
+                    required={true}
+                    variant='outlined'
+                    label='Street Number and Name'
+                    name='street'
+                    value={values.street}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    variant='outlined'
+                    label='Unit Number (if applicable)'
+                    name='unitNumber'
+                    value={values.unitNumber}
+                    onChange={handleInputChange}
+                    />
                     <FormControl>
                         <FormLabel>Type of Housing</FormLabel>
                         <RadioGroup
-                            row
+                        row
                             name='houseType'
                             value={values.houseType}
                             onChange={handleInputChange}>
@@ -89,8 +156,81 @@ export default function Form() {
                         </RadioGroup>
                     </FormControl>
                 </Grid>
+                <Grid item xs={12} className='input'>
+                <TextField
+                    required={true}
+                    variant='outlined'
+                    label='City or Town'
+                    name='city'
+                    value={values.city}
+                    onChange={handleInputChange}
+                    />
+                    <Select onChange={handleInputChange} label="State" value={values.state} name="state" required={true}>
+                        {states.map(item => (
+                            <option>
+                                {item.abbreviation}
+                            </option>
+                        ))}
+                    </Select>
+                    <TextField
+                    required={true}
+                    variant='outlined'
+                    label='ZIP Code'
+                    shrink={true}
+                    name='zipCode'
+                    value={values.zipCode}
+                    onChange={handleInputChange}
+                    />
+                </Grid>
+                <Grid item xs={12} className='input'>
+                    <TextField
+                    required={true}
+                    variant='outlined'
+                    label='Daytime Telephone Number'
+                    name='phone'
+                    value={values.phone}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    required={true}
+                    variant='outlined'
+                    label='Email Address'
+                    shrink={true}
+                    name='email'
+                    value={values.email}
+                    onChange={handleInputChange}
+                    />
+                </Grid>
             </Grid>
-            <Button onClick={routeChange} text='Next'/>
+            <h3>Credit Card Information</h3>
+                <Grid item xs={12} className='input'>
+                <TextField
+                    required={true}
+                    variant='outlined'
+                    label='Credit Card Number'
+                    name='creditNum'
+                    value={values.creditNum}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    variant='outlined'
+                    label='Middle Name (if any)'
+                    name='middleName'
+                    value={values.middleName}
+                    onChange={handleInputChange}
+                    />
+                    <TextField
+                    required={true}
+                    variant='outlined'
+                    label='Family Name'
+                    shrink={true}
+                    name='familyName'
+                    value={values.familyName}
+                    onChange={handleInputChange}
+                    />
+                </Grid>
+            <Button onClick={routeChange} text='Next' className="input"/>
         </form>
+        </Paper>
     )
 }
--- oa/src/Review.js
+++ oa/src/Review.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, Link } from 'react'
-import { FormControl, FormControlLabel, FormLabel, Grid,RadioGroup,TextField, Radio } from '@mui/material';
+import { FormControl, FormControlLabel, FormLabel, Grid,RadioGroup,TextField, Radio, Snackbar, IconButton } from '@mui/material';
 import { useForm } from './components/useForm';
 import {useNavigate, Router, Routes, BrowserRouter} from 'react-router-dom'
 import { Button } from './components/Button'
 import { getForm, setForm } from './services/formService'
-
+import { CloseRounded } from '@mui/icons-material';
 const initialValues = {
     givenNameA: getForm("givenNameA"),
     middleNameA: '',
@@ -41,11 +41,18 @@ export default function Review() {
 
     }=useForm(initialValues, true, validate);
 
+    const [open, setOpen] = React.useState(false);
+ 
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        alert("u good to submit?")
+        setOpen(true);
         setForm(values)
         resetForm()
+        navigate('/')
     }
 
     const navigate = useNavigate();
@@ -53,6 +60,22 @@ export default function Review() {
         let path = '/';
         navigate(path);
     }
+    const action = (
+        <React.Fragment>
+          <Button color="secondary" size="small" onClick={handleClose}>
+            UNDO
+          </Button>
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={handleClose}
+          >
+            <CloseRounded fontSize="small" />
+          </IconButton>
+        </React.Fragment>
+      );
+ 
 
     return (
         <>
@@ -60,6 +83,17 @@ export default function Review() {
             <Button onClick={handleSubmit} type='submit' text='Submit'/>
         <div>
         </div>
+        <Snackbar
+                anchorOrigin={{
+                    horizontal: "left",
+                    vertical: "bottom",
+                }}
+                open={open}
+                autoHideDuration={50000}
+                message="The form was successfully submitted"
+                onClose={handleClose}
+                action={action}
+            />
         </>
     )
     }
